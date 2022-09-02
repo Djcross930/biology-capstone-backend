fdd14af0bd794c02eb1c6435ac3c94e438b0bb95
Dan Cross <dancross96@gmail.com>
2022-09-02 14:15:00 -0500

    added mrna sequence method and a,t,g,c content methods

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -17,4 +17,39 @@ class Gene < ApplicationRecord
     stop_codon.unshift(sequence[-3])
     return stop_codon
   end
+
+  def mrna_sequence
+    sequence = cds_sequence.split("")
+    rna_sequence = []
+    sequence.each do |base|
+      if base == "T"
+        rna_sequence << "U"
+      else
+        rna_sequence << base
+      end
+    end
+    return rna_sequence
+  end
+
+  def c_content
+    sequence = cds_sequence.split("")
+    c = sequence.count("C")
+    c = c.to_f
+    total = sequence.length.to_f
+    c_ratio = (c / total)
+    return c_ratio.to_f 
+  end
+
+  def g_content
+    return c_content()
+  end
+  def a_content
+    total = (1 - (c_content() * 2))
+    g = (total / 2)
+    return g
+  end
+
+  def t_content
+    return a_content()
+  end
 end

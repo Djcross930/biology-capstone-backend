7e6c4556d2d8fbabba9bc03bb59ccdcb0d622f83
Dan Cross <dancross96@gmail.com>
2022-09-02 13:10:44 -0500

    added cors and route in create gene for users and non users (different path)

--- app/controllers/genes_controller.rb
+++ app/controllers/genes_controller.rb
@@ -11,13 +11,26 @@ class GenesController < ApplicationController
   end
 
   def create
-    gene = Gene.new
-    gene.common_name = params[:common_name]
-    info = HTTP.get("https://rest.ensembl.org/xrefs/symbol/homo_sapiens/#{gene.common_name}?content-type=application/json").parse
-    gene.ens_id = info[0]["id"]
-    info_one = HTTP.get("https://rest.ensembl.org/sequence/id/#{gene.ens_id}?type=cds&multiple_sequences=100&content-type=application/json").parse
-    info_one = info_one[0]["seq"]
-    gene.cds_sequence = info_one
-    render json: gene.as_json
+    if current_user
+      gene = Gene.new
+      gene.common_name = params[:common_name]
+      info = HTTP.get("https://rest.ensembl.org/xrefs/symbol/homo_sapiens/#{gene.common_name}?content-type=application/json").parse
+      gene.ens_id = info[0]["id"]
+      info_one = HTTP.get("https://rest.ensembl.org/sequence/id/#{gene.ens_id}?type=cds&multiple_sequences=100&content-type=application/json").parse
+      info_one = info_one[0]["seq"]
+      gene.cds_sequence = info_one
+      gene.user_id = current_user.id
+      gene.save
+      render json: gene.as_json
+    else
+      gene = Gene.new
+      gene.common_name = params[:common_name]
+      info = HTTP.get("https://rest.ensembl.org/xrefs/symbol/homo_sapiens/#{gene.common_name}?content-type=application/json").parse
+      gene.ens_id = info[0]["id"]
+      info_one = HTTP.get("https://rest.ensembl.org/sequence/id/#{gene.ens_id}?type=cds&multiple_sequences=100&content-type=application/json").parse
+      info_one = info_one[0]["seq"]
+      gene.cds_sequence = info_one
+      render json: gene.as_json
+    end
   end
 end

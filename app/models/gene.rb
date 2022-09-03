d405de2bf486b32aa4bf8cc148f0c56f0335e082
Dan Cross <dancross96@gmail.com>
2022-09-03 01:23:47 -0500

    added amino acid sequence method

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -56,4 +56,125 @@ class Gene < ApplicationRecord
   def g_c_content
     return (c_content() * 2)
   end
+
+  def amino_acid_sequence
+    switch = 0
+    sequence = cds_sequence.split("")
+    amino_acid_index = 0
+    base_pair_index = 0
+    protein = []
+    while switch == 0
+      amino_acids = []
+      amino_acids << sequence[amino_acid_index]
+      amino_acids << sequence[amino_acid_index + 1]
+      amino_acids << sequence[amino_acid_index + 2]
+      if  (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "G") 
+        protein << "M"
+        amino_acid_index += 3
+      elsif (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "A")
+        protein << "I"
+        amino_acid_index += 3
+      
+      
+      elsif (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "C")
+        protein << "F"
+        amino_acid_index += 3
+      
+      
+      elsif (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "G") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "G")
+        protein << "L"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "T" && amino_acids[base_pair_index + 2] == "G")
+        protein << "V"
+        amino_acid_index += 3
+    
+      
+      elsif (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "G") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "C")
+        protein << "S"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "G")
+        protein << "P"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "G")
+        protein << "T"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "C" && amino_acids[base_pair_index + 2] == "G")
+        protein << "A"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "C")
+        protein << "Y"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "C")
+        protein << "H"
+        amino_acid_index += 3
+    
+      
+      elsif (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "G")
+        protein << "Q"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "C")
+        protein << "N"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "G")
+        protein << "K"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "C")
+        protein << "D"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "G")
+        protein << "E"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "C")
+        protein << "C"
+        amino_acid_index += 3
+    
+      elsif (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "G")
+        protein << "W"
+        amino_acid_index += 3
+    
+    
+      elsif (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "C" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "G") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "A" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "G") 
+        protein << "R"
+        amino_acid_index += 3
+    
+    
+    
+      elsif (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "T") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "C") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "G" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "G")
+        protein << "G"
+        amino_acid_index += 3
+    
+      
+      elsif (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "A") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "A" && amino_acids[base_pair_index + 2] == "G") || (amino_acids[base_pair_index] == "T" && amino_acids[base_pair_index + 1] == "G" && amino_acids[base_pair_index + 2] == "A")
+        protein << "-"
+        switch = 1
+      end
+    end
+    return protein
+  end
+
+
+
+
 end
